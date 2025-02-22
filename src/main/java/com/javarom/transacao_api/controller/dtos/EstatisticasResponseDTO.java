45e37ecd638c519456c38e1678c2f3eb62371ea8
unknown <rommelprata@hotmail.com>
2025-02-22 16:47:07 -0300

    inicio de projeto

---
+++ src/main/java/com/javarom/transacao_api/controller/dtos/EstatisticasResponseDTO.java
@@ -0,0 +1,9 @@
+package com.javarom.transacao_api.controller.dtos;
+
+public record EstatisticasResponseDTO(Long count,
+                                      Double sum,
+                                      Double avg,
+                                      Double min,
+                                      Double max) {
+
+}
---
+++ src/main/java/com/javarom/transacao_api/controller/dtos/TransacaoRequestDTO.java
@@ -0,0 +1,6 @@
+package com.javarom.transacao_api.controller.dtos;
+
+import java.time.OffsetDateTime;
+
+public record TransacaoRequestDTO(Double valor, OffsetDateTime dataHora) {
+}
